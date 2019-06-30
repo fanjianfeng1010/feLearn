d6f07bb9f30e0c98d5aa8bbc63be416bef2e6fb1
fanjianfeng1010 <264589826@qq.com>
2019-06-30 11:16:46 +0800

    upload ketang project

--- feLearn/react/TODO/spa/src/routes/Home.js
+++ feLearn/react/TODO/spa/src/routes/Home.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { DatePicker, Icon, Button, LocaleProvider, Calendar } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import '../static/antd.css'
+
+import '../static/antd.css';
+
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {}
   }
   render() {
-    return (<div>
-      我是首页
-    </div>);
+    //=> 只要 localProvider 包含的组件都是被汉化的
+    return (<LocaleProvider locale={zh_CN}>
+      <Button></Button>
+    </LocaleProvider>);
   }
 }
 
